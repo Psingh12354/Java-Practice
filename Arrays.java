55d40763417231474acaff969bb8d8c5cbc67cf2
PRIYANSHU SINGH <55645997+Psingh12354@users.noreply.github.com>
2020-06-27 16:01:19 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,13 @@
+
+public class Arrays {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int[] array={1,2,3,4,5};
+			System.out.println(array[1]);
+	}
+
+}
